3fa4d0856ef12ef206bce3de51e7e1bf7befb7cf
vbn930 <vbn930@naver.com>
2025-06-01 00:45:53 +0900

    Add new dto for data dict

--- ExchangeRateClientService/Dtos/ConvertedExchangeRateData.cs
+++ ExchangeRateClientService/Dtos/ConvertedExchangeRateData.cs
@@ -13,4 +13,23 @@ namespace ExchangeRateClientService.Dtos
         [JsonPropertyName("rates")]
         public Dictionary<string, decimal> Rates { get; set; }
     }
+
+    public class ConvertedExchangeRateDataDict
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+        [JsonPropertyName("data")]
+        public Dictionary<string, ConvertedExchangeRateData> Data { get; set; }
+
+        public ConvertedExchangeRateDataDict(string date, Dictionary<string, ConvertedExchangeRateData> data)
+        {
+            Date = date;
+            Data = data;
+            Id = $"ConvertedExchangeRateDataDict-{date}";
+        }
+    }
 }
